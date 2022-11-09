096d00991f6e6a993a7260c3f4e04568ef94a6c8
Travis Parks <travisparks0788@gmail.com>
2022-11-09 16:25:49 -0500

    Add an array of places to visit

--- friends.js
+++ friends.js
@@ -3,3 +3,8 @@ var arrays = ["Joe", "Travis", "Ben", "Kirk", "Kaitlyn"]
 console.log(arrays[2])
 console.log(arrays[4])
 console.log(arrays[0])
+
+var places = ["Tokyo", "Portugal", "Vermont", "Norway", "Banff"]
+
+console.log(places.pop())
+console.log(places)
